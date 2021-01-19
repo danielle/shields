8ecd92950cc5df23ce5d6b754acabafcf1ba05c9
Danielle.Derby@trinet.com <Danielle.Derby@trinet.com>
2021-01-19 17:52:45 -0600

    Remove Improvement from Jira completion and add "Sprint" to completion label

--- services/jira/jira-sprint.service.js
+++ services/jira/jira-sprint.service.js
@@ -73,7 +73,7 @@ module.exports = class JiraSprint extends BaseJsonService {
       color = 'brightgreen'
     }
     return {
-      label: 'completion',
+      label: 'sprint completion',
       message: `${percentComplete.toFixed(0)}%`,
       color,
     }
@@ -89,7 +89,7 @@ module.exports = class JiraSprint extends BaseJsonService {
         schema,
         options: {
           qs: {
-            jql: `sprint=${sprintId} AND type IN (Bug,Improvement,Story,"Technical task")`,
+            jql: `sprint=${sprintId} AND type IN (Bug,Story,"Technical task")`,
             fields: 'resolution',
             maxResults: 500,
           },
